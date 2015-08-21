7d7de06288ec288e31bf44246bf0360e2283c556
nauval2007 <nauval2007@gmail.com>
2015-08-21 18:44:48 +0700

    Fri, Aug 21, 2015 18:44:39

--- centos6.sh
+++ centos6.sh
@@ -145,13 +145,15 @@ cd
 # setting port ssh
 echo "Port 143" >> /etc/ssh/sshd_config
 echo "Port  22" >> /etc/ssh/sshd_config
-sed -i 's/#Banner/Banner/g' /etc/ssh/sshd_config
+sed -i 's/#Banner/Banner /etc/issue.net/g' /etc/ssh/sshd_config
 service sshd restart
 chkconfig sshd on
 
 # install dropbear
 yum -y install dropbear
-echo "OPTIONS=\"-p 109 -p 110 -p 443\"" > /etc/sysconfig/dropbear
+# centos
+echo "OPTIONS=\"-b /etc/issue.net -p 109 -p 110 -p 443\"" > /etc/sysconfig/dropbear
+
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS=""/g' /etc/default/dropbear
